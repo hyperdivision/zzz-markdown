f808fa528eb903cc1485abde66644a3a10744e20
Emil Bay <github@tixz.dk>
2020-12-31 02:22:31 +0100

    Immitate marked parser

--- index.js
+++ index.js
@@ -10,47 +10,90 @@ module.exports = z(({ life }, [text]) => {
     })
   })
 
-  const tokens = markdown.lexer(text)
+  const tokens = markdown.Lexer.lex(text, markdown.defaults)
 
-  return (attrs) => z``(attrs, walk(tokens))
+  return (attrs) => z``(attrs, tokens.map(parse))
 })
 
-const code = z`pre.hljs`
-const codespan = z`code.hljs-inline`
+const slugger = new markdown.Slugger()
+const hr = z`hr`
+const html = z.trust
+const code = ({ lang, escaped }, text) => z`pre.hljs`(highlight(text, lang))
+const codespan = text => z`code.hljs-inline`(highlight(text))
+const blockquote = z`blockquote`
+const paragraph = z`p`
+const br = z`br`
+const link = z`a`
+const image = z`img`
+const strong = z`strong`
+const em = z`em`
+const del = z`del`
+const ol = z`ol`
+const ul = z`ul`
+const li = z`li`
+const error = (token) => { throw token }
 
-function walk (token) {
-  if (Array.isArray(token)) return token.map(walk)
-
+function parse (token, top = true) {
   switch (token.type) {
-    case 'space': return z`br`
-    case 'text': return token.tokens ? walk(token.tokens) : token.raw
-    case 'codespan': return codespan(highlight(token.text, token.lang))
-    case 'code': return code(highlight(token.text, token.lang))
-    case 'em': return z`em`(walk(token.tokens))
-    case 'strong': return z`strong`(walk(token.tokens))
-    case 'del': return z`del`(walk(token.tokens))
-    case 'heading': return z`h${token.depth}`(walk(token.tokens))
-    case 'blockquote': return z`blockquote`(walk(token.tokens))
-
-    case 'paragraph': return z`p`(walk(token.tokens))
-    case 'table': return z`table`([
-      z`thead`(token.tokens.header.map((t, i) => z`th`({ align: token.align[i] }, walk(t)))),
-      z`tbody`(token.tokens.cells.map((t, _) =>
-        z`tr`(t.map((c, i) =>
-          z`td`({ align: token.align[i] }, walk(c))
-        ))
-      ))
-    ])
-    case 'link': return z`a`({ href: token.href, title: token.title }, walk(token.tokens))
-    case 'image': return z`img`({ title: token.title, src: token.href, alt: token.text })
-    case 'list': return z`${token.ordered ? 'ol' : 'ul'}`(walk(token.items))
-    case 'list_item': return z`li`(walk(token.tokens))
-    default: return token
+    case 'space': return ''
+    case 'hr': return hr
+    case 'heading': return z`h${token.depth}`({ id: slugger.slug(token.text) }, token.tokens.map(parseInline))
+    case 'code': return code({ lang: token.lang, escaped: token.escaped }, token.text)
+    case 'table': return table(token)
+    case 'blockquote': return blockquote(token.tokens.map(parse))
+    case 'list': return list(token)
+    case 'html': return html(token.raw)
+    case 'paragraph': return paragraph(token.tokens.map(parseInline))
+    case 'text': {
+      // TODO needs to keep parsing next text tokens and bundle into one paragraph
+      const body = token.tokens ? token.tokens.map(parseInline) : token.raw
+      return top ? paragraph(body) : body
+    }
+    default: return error(token)
   }
+}
+
+function table ({ align, tokens }) {
+  return z`table`(
+    z`thead`(tokens.header.map((th, i) =>
+      z`th`({ align: align[i] }, parseInline(th))
+    )),
+    z`tbody`(
+      tokens.cells.map((tr) =>
+        z`tr`(tr.map((td, i) =>
+          z`td`({ align: align[i] }, parseInline(td))
+        ))
+      )
+    )
+  )
+}
+
+function list ({ ordered, start, loose, items }) {
+  return (ordered ? ol : ul)({ start }, items.map(item =>
+    li(item.tokens.map(t =>
+      parse(t, loose)
+    ))
+  ))
 }
 
 function highlight (code, lang) {
   if (lang) return z.trust(hljs.highlight(lang, code).value)
 
   return z.trust(hljs.highlightAuto(code).value)
+}
+
+function parseInline (token) {
+  switch (token.type) {
+    case 'escape': return token.raw
+    case 'html': return html(token.raw)
+    case 'codespan': return codespan(token.text)
+    case 'br': return br
+    case 'link': return link({ href: token.href, title: token.title }, token.tokens.map(parseInline))
+    case 'image': return image({ src: token.href, title: token.title, alt: token.text })
+    case 'strong': return strong(token.tokens.map(parseInline))
+    case 'em': return em(token.tokens.map(parseInline))
+    case 'del': return del(token.tokens.map(parseInline))
+    case 'text': return token.raw
+    default: return error(token)
+  }
 }
